ab7b95140811ae3e74f85da58c8a4a0adf29a579
Skinny_Vin <118040316+SkinnyVin1@users.noreply.github.com>
2023-05-12 14:41:08 +0800

    loader

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
 });
+
+
+
+// LOADER===================================
+// Get the loader element
+const loader = document.querySelector('.loader');
+
+// Show the loader
+function showLoader() {
+  loader.style.display = 'flex';
+}
+
+// Hide the loader
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+// Call showLoader() before your content loads
+showLoader();
+
+// Call hideLoader() when your content has finished loading
+window.addEventListener('load', () => {
+  hideLoader();
+});
